c685a06907971e06445f61c58e2452166f7ce59e
Sneaken <924393527@qq.com>
2023-02-06 12:20:40 +0800

    example: useEffect 只有在 render 的时候才会去判断是否满足条件

---
+++ app/src/examples/Effect.js
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from "react";
+
+function Effect() {
+  const ref = useRef(0);
+  const [_, update] = useState({});
+  useEffect(() => {
+    const timer = setInterval(() => {
+      ref.current = (ref.current ?? 0) + 1;
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("ref.value =>", ref.current);
+  }, [ref.current]);
+
+  return (
+    <div>
+      <h3 onClick={() => update({})}>Effect</h3>
+      {ref.current}
+    </div>
+  );
+}
+
+export default Effect;
