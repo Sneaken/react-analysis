6a2f21867796c3ef027fb9bd38b3a788d0c94655
Sneaken <924393527@qq.com>
2023-02-08 17:02:47 +0800

    note: React Mode 备注

--- app/src/react/packages/react-reconciler/src/ReactTypeOfMode.js
+++ app/src/react/packages/react-reconciler/src/ReactTypeOfMode.js
@@ -10,10 +10,16 @@
 export type TypeOfMode = number;
 
 export const NoMode = /*                         */ 0b000000;
+/* 并发模式 */
 // TODO: Remove ConcurrentMode by reading from the root tag instead
 export const ConcurrentMode = /*                 */ 0b000001;
+/* 性能分析模式 */
 export const ProfileMode = /*                    */ 0b000010;
+// 调试模式
 export const DebugTracingMode = /*               */ 0b000100;
+// StrictMode
 export const StrictLegacyMode = /*               */ 0b001000;
+// StrictMode && enableStrictEffects
 export const StrictEffectsMode = /*              */ 0b010000;
+// 并发模式下 lane 使用被赋予的 lane 模式
 export const ConcurrentUpdatesByDefaultMode = /* */ 0b100000;
