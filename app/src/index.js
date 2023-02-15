eee4cb6466fdda08c63b1f4c7e949b912f8f2087
Sneaken <924393527@qq.com>
2023-02-15 22:11:12 +0800

    note: 可以通过参数形式开启 StrictMode

--- app/src/index.js
+++ app/src/index.js
@@ -3,10 +3,9 @@ import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const options = {
+  unstable_strictMode: false,
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"), options);
 root.render(<App />);
